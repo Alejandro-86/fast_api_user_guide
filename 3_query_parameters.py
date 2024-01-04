cd9a5d838b0a7d345b9406a52e426027620f8d39
Alex_alonso_86 <aalonsocanales@gmail.com>
2024-01-04 16:02:40 +0000

    Refactor files

---
+++ 3_query_parameters.py
@@ -0,0 +1,25 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/test/")
+def read_item_test(pos: int = 0):
+    return [pos, fake_items_db[pos]]
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
